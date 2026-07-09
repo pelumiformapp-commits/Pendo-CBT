bcea839afabbc781ec784e23574177631b9cf5d1
pelumiformapp-commits <pelumiformapp@gmail.com>
2026-07-09 06:27:00 +0100

    Create questions.js

---
+++ js/questions.js
@@ -0,0 +1,52 @@
+const questions = [
+{
+question: "What does CPU stand for?",
+options: [
+"Central Processing Unit",
+"Computer Personal Unit",
+"Central Program Utility",
+"Computer Processing Unit"
+],
+answer: 0
+},
+{
+question: "Which language is used to style web pages?",
+options: [
+"Python",
+"CSS",
+"Java",
+"C++"
+],
+answer: 1
+},
+{
+question: "HTML stands for?",
+options: [
+"Hyper Text Markup Language",
+"High Transfer Machine Language",
+"Hyperlink Text Machine Language",
+"Home Tool Markup Language"
+],
+answer: 0
+},
+{
+question: "Which symbol is used for comments in JavaScript?",
+options: [
+"//",
+"<!-- -->",
+"##",
+"**"
+],
+answer: 0
+},
+{
+question: "Which company developed JavaScript?",
+options: [
+"Microsoft",
+"Netscape",
+"Google",
+"Apple"
+],
+answer: 1
+}
+];
